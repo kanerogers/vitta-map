26e945dff2438fa471036522bcaaeb91b716b086
Kane Rogers <kane.rogers@bluereef.com.au>
2013-08-10 19:32:35 +1000

    More work.

--- app.js
+++ app.js
@@ -78,14 +78,24 @@ schoolsInStates = [
     schools: [
       {
         name: "Camberwell Grammar",
-        logo: "http://catholicschoolsguide.com.au/wp-content/uploads/xavier-college1.jpg",
+        logo: "camberwell",
         lat: -37.8169075,
         lng: 145.0682967
       }, {
         name: "Xavier College",
-        logo: "http://catholicschoolsguide.com.au/wp-content/uploads/xavier-college1.jpg",
+        logo: "xavier",
         lat: -37.812269,
         lng: 145.031685
+      }, {
+        name: "Glen Waverley Secondary College",
+        logo: "gwsc",
+        lat: -37.8780251,
+        lng: 145.16218
+      }, {
+        name: "Fintona Girls' School",
+        logo: "fintona",
+        lat: -37.8151431,
+        lng: 145.079504
       }
     ]
   }, {
@@ -95,17 +105,165 @@ schoolsInStates = [
     lng: 121.6283098,
     schools: [
       {
-        name: "Mazenod College",
-        logo: "http://catholicschoolsguide.com.au/wp-content/uploads/xavier-college1.jpg",
+        name: "John XXIII College",
+        logo: "johnxxii",
         lat: -32.006682,
         lng: 116.038843
       }, {
         name: "Perth College",
-        logo: "http://catholicschoolsguide.com.au/wp-content/uploads/xavier-college1.jpg",
+        logo: "perth",
         lat: -31.931425,
         lng: 115.875297
+      }, {
+        name: "Penrhos College",
+        logo: "penrhos",
+        lat: -31.9971921,
+        lng: 115.8721388
+      }, {
+        name: "Aquinas College",
+        logo: "aquinas",
+        lat: -32.024612,
+        lng: 115.865683
       }
     ]
+  }, {
+    name: "South Australia",
+    range: 1800000,
+    lat: -29.9270476726815,
+    lng: 138.20333108807867,
+    schools: [
+      {
+        name: "Concordia College",
+        logo: "concordia",
+        lat: -34.9595118,
+        lng: 138.6173848
+      }, {
+        name: "Pulteney Gramar",
+        logo: "pulteney",
+        lat: -34.9352115,
+        lng: 138.6024578
+      }, {
+        name: "St. George's College",
+        logo: "stgeorge",
+        lat: -34.9223551,
+        lng: 138.570423
+      }, {
+        name: "Trinity College, Gawler",
+        logo: "trinity",
+        lat: -34.6386448,
+        lng: 138.6464245
+      }
+    ]
+  }, {
+    name: "New South Wales",
+    range: 1750000,
+    lat: -27.72857752890055,
+    lng: 147.16564159039075,
+    schools: {
+      name: "Kambala",
+      logo: "kambala",
+      lat: -33.8647888,
+      lng: 151.2715316,
+      name: "SCEGGS Darlinghurst",
+      logo: "sceggs",
+      lat: -33.876144,
+      lng: 151.218369,
+      name: "St. Aloysius' College",
+      logo: "aloysius",
+      lat: -33.8484373,
+      lng: 151.2143868,
+      name: "Presbyterian Ladies College, Sydney",
+      logo: "plc",
+      lat: -33.881386,
+      lng: 151.1154961
+    }
+  }, {
+    name: "Queensland",
+    range: 2457255,
+    lat: -18.846606463003678,
+    lng: 145.39413779269196,
+    schools: {
+      name: "Rockhampton Grammar",
+      logo: "rockhampton",
+      lat: -23.3851158,
+      lng: 150.4940152,
+      name: "A.B. Paterson College",
+      logo: "abpat",
+      lat: -27.9270641,
+      lng: 153.3601713,
+      name: "Citipointe Christian College",
+      logo: "citipointe",
+      lat: -27.534798,
+      lng: 153.113118,
+      name: "St. John's Anglican College",
+      logo: "stjohns",
+      lat: -27.6125928,
+      lng: 152.961073
+    }
+  }, {
+    name: "Northern Territory",
+    range: 2457255,
+    lat: -20.09994393657499,
+    lng: 134.62769228738617,
+    schools: {
+      name: "St. Philip's College",
+      logo: "stphilips",
+      lat: -23.690934,
+      lng: 133.882398,
+      name: "Kormilda College",
+      logo: "kormilda",
+      lat: -12.4436,
+      lng: 130.9229
+    }
+  }, {
+    name: "Australian Capital Territory",
+    range: 16722.5264822886,
+    lat: -35.29642006461715,
+    lng: 149.140001506044,
+    schools: {
+      name: "Radford College",
+      logo: "radford",
+      lat: -35.2438889,
+      lng: 149.0880556,
+      name: "Marist College",
+      logo: "marist",
+      lat: -35.3597768,
+      lng: 149.0884452
+    }
+  }, {
+    name: "South East Asia",
+    range: 4400000,
+    lat: 11.801464426569357,
+    lng: 102.61731840153027,
+    schools: {
+      name: "International School of Kuala Lumpur",
+      logo: "iskl",
+      lat: 3.161711,
+      lng: 101.7557627,
+      name: "Jakarta International School",
+      logo: "jis",
+      lat: -6.28564,
+      lng: 106.78803,
+      name: "Bangkok Patana School",
+      logo: "patana",
+      lat: 13.6617875,
+      lng: 100.6221838
+    }
+  }, {
+    name: "China",
+    range: 4400000,
+    lat: 33.4148047064862,
+    lng: 97.74769944153013,
+    schools: {
+      name: "Nanjing International School",
+      logo: "nanjing",
+      lat: 32.0791939,
+      lng: 118.7511441,
+      name: "Tianjin International School",
+      logo: "tianjin",
+      lat: 39.0559337,
+      lng: 117.2724261
+    }
   }
 ];
 
